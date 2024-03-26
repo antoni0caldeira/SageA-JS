69e3c974489eb9b421a3d3909336787c03e8c388
Antonio Caldeira <antoniocaldeira@yahoo.com>
2024-03-26 15:15:24 +0000

    Number One

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Welcome to the Sage A Javascript');
+let greeting = 'Hello';
+let audience = 'Mother Fs...';
+
+// Outut to console greeting plus audience
+
+console.log(greeting + ' ' + audience);
+
+console.log(`${greeting} ${audience}`);
+
+// A variable is like a storage unit. You can keep diferent things in it and change it whenever you want.
